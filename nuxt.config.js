85c98fcb022eb130e5b18827eebae6a115828e43
shyam <shyammistry96@gmail.com>
2020-12-11 21:39:02 +0000

    build: faff with docs

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,10 @@ export default {
     // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
     plugins: [
     ],
-
+    
+    router: {
+        base: '/NuxtDemo/'
+    },
     // Auto import components (https://go.nuxtjs.dev/config-components)
     components: true,
 
