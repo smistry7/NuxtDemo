f8d7216224bd65c5d3374c8bcc548f639c31f397
shyam <shyammistry96@gmail.com>
2020-12-11 21:31:48 +0000

    chore: add docs?

---
+++ docs/_nuxt/779151d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{299:function(t,e,n){"use strict";n.r(e);var l=n(53),component=Object(l.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"alert alert-info"},[e("p",[this._t("info-box",[this._v("default")])],2)])}),[],!1,null,null,null);e.default=component.exports}}]);
--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,9 @@ export default {
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
         ]
     },
-
+    generate: {
+        dir: 'docs'
+    },
     // Global CSS (https://go.nuxtjs.dev/config-css)
     css: [
     ],
